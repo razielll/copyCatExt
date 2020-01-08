9a64c5be204a3c8ee453937bb40a848d65b898cf
razielll <asto1387@gmail.com>
2020-01-07 18:38:08 -0600

    ready to publish

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
 "use strict";
 
-console.log('%c ALT+CLICK copy and append to clipboard', 'color: #cc00cc;, padding: 16px; font-size: 20px; font-weight: bold');
+window.onload = function () {
+    console.log('%cALT+CLICK any text to copy and append to clipboard', 'color: #cc00cc;, padding: 16px; font-size: 20px; font-weight: bold');
 
-var TXTS = [];
-document.addEventListener('click', function altCopyFunction(e) {
-    if (!e.altKey || !e.target.textContent) {
-        return;
-    };
-    e.stopPropagation(); // We dont wan't other events firing
-    e.preventDefault(); // Prevents link/acnhor opening on alt click
-
-    var txt = e.target.textContent;
-
-    var toggleOn = e.target.classList.toggle('tooltip');
-
-    console.log('toggleOn', toggleOn);
-
-    !toggleOn ? removeItem(txt) : TXTS.push(txt);
-
-    copyText();
-}, {
-    passive: false
-});
-
-function removeItem(txt) {
-    TXTS.forEach(function (val, idx) {
-        var equal = val === txt ? true : false;
-        if (equal) {
-            TXTS.splice(idx, 1);
-        }
+    let TXTS = [];
+    document.body.addEventListener('click', handleClickForCopy, {
+        capture: true,
+        passive: false
     });
-};
 
-function copyText() {
-    var inputEl = document.createElement('input');
-    document.body.appendChild(inputEl);
-    inputEl.classList.add('hidden-input-txt');
-    inputEl.value = TXTS.join(' ') || ' ';
-    inputEl.select();
-    document.execCommand("copy");
-    return setTimeout(function () {
-        inputEl.remove();
-    }, 666);
-};
+    let removeTxt = currTxt => TXTS = TXTS.filter(txt => currTxt !== txt);
+
+
+    function handleClickForCopy(e) {
+        if (!e.altKey || typeof e.target.textContent !== 'string') return;
+
+        e.stopPropagation(); // We dont wan't other events firing
+        e.preventDefault(); // Prevents link/acnhor opening on alt click
+        let txtContent = e.target.textContent;
+
+        let isToggle = e.target.classList.toggle('tooltip');
+
+        isToggle ? TXTS.push(txtContent) : removeTxt(txtContent);
+
+        copyText();
+    };
+
+    function copyText() {
+        let inputEl = document.createElement('input');
+        inputEl.classList.add('temp-input-holder');
+        document.body.appendChild(inputEl);
+        inputEl.value = TXTS.join(' ') || ' ';
+        inputEl.select();
+        document.execCommand("copy");
+        return setTimeout(function () {
+            inputEl.remove();
+        }, 50);
+    };
+}();
